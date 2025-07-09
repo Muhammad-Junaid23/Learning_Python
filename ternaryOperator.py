cb55ea8af81b50abfd504756e3c7f9075af57bd0
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2025-07-09 22:27:39 +0500

    ternary operator

---
+++ ternaryOperator.py
@@ -0,0 +1,14 @@
+# we will look at the ternary operator in python
+
+age = 22
+
+# if age > 18:
+#     message = "Eligible"
+# else:
+#     message = 'Not Eligible'
+
+# age = 30 if age >= 18 else 15
+message = "Eligible" if age >= 18 else "Not Eligible"
+
+# print(age)
+print(message)
