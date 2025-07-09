5799c07270bda36dbde4cb0f9f784a925eb926ac
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2025-07-09 11:12:14 +0500

    basic maths on numbers

---
+++ mathsOnNumbers.py
@@ -0,0 +1,18 @@
+# we can perform basic mathematical operations on numbers
+# for example +,-,*,/,%
+
+print(19+5)
+print(19-5)
+print(19/5)
+print(19//5)  # it will remove the decimal values (floating values)
+print(19*5)
+print(19 % 5)
+print(19**5)  # it is used for power
+
+
+x = 76
+x = x + 2
+print("value of : ", x)
+
+x *= 4  # this is augmented assignment operator
+print("value of x using augmented assignment : ", x)
