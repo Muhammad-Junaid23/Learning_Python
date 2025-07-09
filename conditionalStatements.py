243efd5a9cd5f7623a690e8dc2d3c5eca087b212
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2025-07-09 12:49:34 +0500

    conditional statements

---
+++ conditionalStatements.py
@@ -0,0 +1,11 @@
+# we will look at the conditional statements in python  like if else
+
+temperature = 22
+if temperature > 30:
+    print("It's melting")
+    print("Let's have an ice cream")
+elif temperature > 20:
+    print("It's perfect for a hike")
+else:
+    print("It's cold outside")
+print("We are out of the conditional statements")
