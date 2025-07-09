fd8718052f0f4c117d27bb726859c0db63d72df2
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2025-07-09 11:22:38 +0500

    math methods using math module

---
+++ mathMethods.py
@@ -0,0 +1,10 @@
+# we can use math methods by importing a math module
+
+import math
+
+print(round(7.323))   # will round off the number
+print(abs(-4.323))     # will return absolute value
+
+print(math.floor(3.98))
+print(math.ceil(4.18))
+print(math.cbrt(3.98))  # will return the cube root
