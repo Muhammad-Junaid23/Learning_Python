c69a354bbc660148a96b70f21b19250862d70b35
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2025-07-09 10:37:36 +0500

    learned formated strings

---
+++ formatedStrings.py
@@ -0,0 +1,15 @@
+# formated strings are similar to back ticks in javascript
+# syntax : f"{variable_one } {variable_two}"
+
+
+# previously we use below syntax but it is ugly
+# first_name = "Star"
+# second_name = "Alone"
+# full_name = "Star" + " " + "Alone"
+# print(full_name)
+
+# now we use formated strings
+first_name = "Unknown"
+last_name = "404"
+full_name = f"{first_name} X {last_name}"
+print(full_name)
