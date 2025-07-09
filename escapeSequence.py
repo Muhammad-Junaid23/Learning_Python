efc8651287a58ce8d4516b6b6e573119982a0b3b
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2025-07-09 10:28:03 +0500

    learned about escape sequence

---
+++ escapeSequence.py
@@ -0,0 +1,9 @@
+# basically escape sequence consist of a escape character '\' and a letter or symbol
+# for example '\n is used for line break'
+# \" is used to add a double quote
+
+subject = "Searching for job opening in your \"home town\" amd looking forward to a meetup"
+print(subject)
+
+line_break = "I am writing this \n to demonstrate the use of line break escape sequence"
+print(line_break)
